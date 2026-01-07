534a83dda9f212879cd7469d563f2241fce96db0
OrientDanssmorePeul <orientdanssmore25p@forward.edu.my>
2026-01-07 11:09:10 +0800

    Implement CORS support and enhance login and product management features

--- Back-End/index.js
+++ Back-End/index.js
@@ -6,6 +6,18 @@ const userRoutes = require("./routes/UserRoute")
 const app = express()
 app.use(express.json())
 
+const cors = require("cors");
+
+const corsHandler = cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200,
+    preflightContinue: true,
+});
+
+app.use(corsHandler)
+
 mongoose
     .connect('mongodb://localhost:27017/jwt_with_products')
     .then(() => {
--- Front-End/jwt-frontend/src/Login.jsx
+++ Front-End/jwt-frontend/src/Login.jsx
@@ -1,22 +1,73 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
 const Login = () => {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [showError, setShowError] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    
+    if(validation()){
+      axios
+        .post("http://localhost:3000/user/login", {email: email, password: password})
+        .then((response)=>{
+          localStorage.setItem("jwt_token", response.data.token)
+          navigate("/products");
+        })
+        .catch((error)=>{
+          console.log(error)
+        })       
+    }else{
+      return
+    }
+  }
+
+  const validation = () => {
+    if(password == "" || email == ""){
+      setError("Please Enter Field")
+      setShowError(true)
+      setTimeout(() => {
+        setShowError(false)
+      }, 5000);
+      return false
+
+    }else {
+      return true
+    }
+  }
+
   return (
     <div className="login-container">
+      <p style={{display: showError ? "block" : "none"}} className="error-message">
+        {error}
+      </p>
       <form className="login-box">
         <h2>Sign In</h2>
 
         <input
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
           type="email"
           placeholder="Email"
           className="login-input"
+          required
         />
 
         <input
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}  
           type="password"
           placeholder="Password"
           className="login-input"
         />
 
-        <button type="submit" className="login-button">
+        <button onClick={handleLogin} type="submit" className="login-button">
           Login
         </button>
       </form>
